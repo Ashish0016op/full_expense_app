73944f49c38420e8063a8931fbecd5c8ccb2fa2c
Ashish0016op <raccoonop0016@gmail.com>
2023-08-22 10:58:56 +0530

    premium_commit

--- controllers/AllData.js
+++ controllers/AllData.js
@@ -1,34 +1,28 @@
 const expenseDetails = require('../model/expenseData');
 const loginDetails = require('../model/loginDetails');
 const sequelize = require('../util/database');
+//const { expenseDetails, loginDetails, sequelize } = require('../models'); // Adjust the paths as needed
 
 exports.getAllData = async (req, res, next) => {
     try {
-        const loginUser = await loginDetails.findAll();
-        const expenseDetail = await expenseDetails.findAll();
-
-        const userExpenses = {};
-
-        expenseDetail.forEach((expense) => {
-            if (userExpenses[expense.signupDatumId]) {
-                userExpenses[expense.signupDatumId] += expense.expense_amount;
-            } else {
-                userExpenses[expense.signupDatumId] = expense.expense_amount;
-            }
+        const leaderboardData = await loginDetails.findAll({
+            attributes: [
+                'id',
+                'Username',
+                [
+                    sequelize.fn('SUM', sequelize.col('expense_details.expense_amount')),
+                    'totalExpenses'
+                ]
+            ],
+            include: [
+                {
+                    model: expenseDetails,
+                    attributes: []
+                }
+            ],
+            group: ['signupdata.id'],
+            order: [[sequelize.fn('SUM', sequelize.col('expense_details.expense_amount')), 'DESC']]
         });
-
-        const leaderboardData = [];
-        loginUser.forEach((user) => {
-            const userId = user.id;
-            const userName = user.Username;
-            const totalExpenses = userExpenses[userId] || 0;
-
-            leaderboardData.push({
-                userName,
-                totalExpenses
-            });
-        });
-        leaderboardData.sort((a, b) => b.totalExpenses - a.totalExpenses);
 
         res.status(200).json(leaderboardData);
     } catch (err) {
@@ -36,4 +30,5 @@ exports.getAllData = async (req, res, next) => {
         res.status(500).json({ error: 'Server error' });
     }
 };
+
 
