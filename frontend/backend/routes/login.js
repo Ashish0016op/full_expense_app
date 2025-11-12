3c0bd65a831dea8d448e70c03cc256bba9a0cfce
Ashish0016op <raccoonop0016@gmail.com>
2025-11-12 20:28:32 +0530

    updated

--- frontend/backend/routes/login.js
+++
@@ -1,6 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const loginDetails=require('../controllers/login');
-router.post('/login',loginDetails.login);
-router.get('/user_login',loginDetails.getDetails);
-module.exports=router;
