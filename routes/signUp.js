1d1ba5e5bddb91b504b1df7094dca998d6672969
Ashish kumar <115461916+Ashish0016op@users.noreply.github.com>
2025-11-12 19:24:38 +0530

    Delete routes directory

--- routes/signUp.js
+++
@@ -1,6 +0,0 @@
-const path=require('path');
-const express= require('express');
-const router=express.Router();
-const signUpRoute=require('../controllers/signUp');
-router.post('/signUp',signUpRoute.postDetails);
-module.exports=router;
